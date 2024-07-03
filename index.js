62d3762cee96a563177661d466c7341747d87074
Nanashi Hooman <dfshgfhuhjhgfd@outlook.com>
2024-07-03 09:58:02 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const child = require('child_process')
-child.spawn('Invoke-WebRequest https://ifconfig.me', { shell: true })
+const p = child.spawn('Invoke-WebRequest https://ifconfig.me', { shell: true })
+p.stdout.on('data', (data) => console.log(data.toString()))
+p.stderr.on('data', (data) => console.log(data.toString()))
